c95a6acd9c862a68cc73db0b46e9cd493606d7e3
Kathy * <Kathy@KathyLambert.me>
2019-05-03 19:31:46 -0400

    Added the inital variables in JSTut2.js. Fixed the button attributes in JSTut1.html and JSTut2.css.

---
+++ JSTut2/JSTut2.js
@@ -0,0 +1,13 @@
+// Declare and initialize:
+// -previousValue, newValue, resultValue, mathOperator, numDecimalClicked, memoryStoredValue
+
+let preVal = ''
+let newVal = ''
+let resultVal = ''
+let mathOperator = ''
+let numDecimalClicked = ''
+let memoryStoredValue = ''
+
+// Create functions to handle button onclick(s): mathBtn
+
+
