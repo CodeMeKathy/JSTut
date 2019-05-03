ddebe62525d005a7fc715177896b35dab94ffad5
Kathy * <Kathy@KathyLambert.me>
2019-05-03 19:49:47 -0400

    Added the functionality for copyBtnPress, pasteBtnPress, and clearBtnPress in JSTut2.js.

--- JSTut2/JSTut2.js
+++ JSTut2/JSTut2.js
@@ -5,9 +5,41 @@ let preVal = ''
 let newVal = ''
 let resultVal = ''
 let mathOperator = ''
-let numDecimalClicked = ''
+let decimalClicked = ''
 let memoryStoredValue = ''
 
 // Create functions to handle button onclick(s): mathBtn
 
 
+function copyBtnPress(num) {
+  memoryStoredValue = document.getElementById('calcInputWindow').value
+}
+
+function pasteBtnPress(params) {
+  if(memoryStoredValue) {
+    document.getElementById('calcInputWindow').value
+    newVal = memoryStoredValue
+  }
+}
+
+function mathBtnPress(operator) {
+  
+}
+
+function numBtnPress(num) {
+  
+}
+
+function equalBtnPress(num) {
+  
+}
+
+function clearBtnPress() {
+  // Reset back to defaults
+  let preVal = ''
+  let newVal = ''
+  let resultVal = ''
+  let mathOperator = ''
+  let decimalClicked = 'false'
+  document.getElementById('calcInputWindow').value = 0
+}
